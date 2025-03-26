0dd3994ab3636abd265e98fd2cc86bf48d9b7b35
Madhav Pathak <maddyp238@gmail.com>
2025-03-26 22:20:13 +0530

    Changes are commited

--- src/test/java/com/orangehrm/pages/LoginPage.java
+++ src/test/java/com/orangehrm/pages/LoginPage.java
@@ -5,27 +5,32 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.orangehrm.test.BaseTest;
-
+import com.reports.ExtentReportUtils;
 public class LoginPage {
-
+	
 	// username, password, login button
 	@FindBy(name = "username")
 	WebElement userNameTextbox;
-
+	
 	@FindBy(name = "password")
 	WebElement passwordTextbox;
-
+	
 	@FindBy(xpath = "//button[@type='submit']")
 	WebElement submitBtn;
-
+	
 	public LoginPage() {
 		PageFactory.initElements(BaseTest.driver, this);
 	}
-
+	
 	public void loginToApp(String username, String password) {
 		userNameTextbox.sendKeys(username);
+		ExtentReportUtils.addStep("Username is entered");
+		
 		passwordTextbox.sendKeys(password);
+		ExtentReportUtils.addStep("Password is entered");
+		
 		submitBtn.click();
+		ExtentReportUtils.addStep("Login button is clicked");
 	}
-
+	
 }
